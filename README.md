52cd8a13926acce120157127b35cbc298d2da382
CirKu17 <CirKu17@gmail.com>
2013-02-21 23:32:41 +0100

    trying bash compatibility, removed Dropbox predisposition

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-_PSyC_ (Passphrases Synchronization CLI) is meant to store an XML password database protected with a PGP key and synchronize it with Dropbox.
+ _PSyC_ (Passphrases Synchronization CLI) is meant to store an XML password database protected with a PGP key and synchronize it with your preferred backup solution (e.g. dropbox).
 
-Is written in perl (using the Template Toolkit) and the CLI is managed by zsh.
+Is written in perl (using the Template Toolkit) and the CLI is managed by zsh/bash.
 
 Initialize
 ----------
 
 To start adding elements to the db, encrypt the empty pwstorage_db.xml in pwstorage_db.xml.gpg .
 
-Needs xml_grep and xclip (may need to be aliased to xclip -selection clipboard).
+xclip may need to be aliased to xclip -selection clipboard in your zshrc/bashrc .
+
+Dependencies
+------------
+
+    * gpg
+    * xml_grep
+    * xclip
+    * shred
+    * perl and the Template module (Template Toolkit)
 
 Aliases
 -------
@@ -17,5 +26,10 @@ Aliases
     psyc-see :       opens the db with less (read-only)
     psyc-copy <id> : copies the passphrase relative to <id> in the clipboard
     psyc-mod :       opens the db with nano (read/write)
-    psyc-sync :      copies the db in local Dropbox-synchronized directory
+    psyc-sync :      copies the db in the preferred backup location
 ```
+
+TODO
+----
+
+* test bash compatibility
--- psyc.sh
+++ psyc.sh
@@ -2,6 +2,7 @@
 
 PSYCPATH=psyc_dir_path
 PSYCHOPATH=gpg_key_user_ID
+MENTALBACKUP=dropbox_or_w/e_path
 
 function psyc-copy() {
 
@@ -10,30 +11,30 @@ function psyc-copy() {
         echo "usage $0 <target id of pwstorage db>"
     else
         echo "[*] Copying to clipboard.."
-        gpg --yes -o /tmp/pwstorage_db.xml -d $PSYCPATH/pwstorage_db.xml.gpg
+        gpg --quiet --yes -o /tmp/pwstorage_db.xml -d $PSYCPATH/pwstorage_db.xml.gpg
         xml_grep "target[@id=\"$1\"]/passphrase" /tmp/pwstorage_db.xml --strict --text_only | xclip
     fi
 
-    shred -u /tmp/pwstorage_db.xml
+    shred /tmp/pwstorage_db.xml && rm /tmp/pwstorage_db.xml
 }
 
 function psyc-add() {
 
     gpg --yes -o /tmp/pwstorage_db.xml -d $PSYCPATH/pwstorage_db.xml.gpg
-    $PSYCPATH
+    cd $PSYCPATH
     perl pwstorage.pl
     gpg --yes -o $PSYCPATH/pwstorage_db.xml.gpg -e -R $PSYCHOPATH /tmp/pwstorage_db.xml
-    $OLDPWD
-    shred -u /tmp/pwstorage_db.xml
+    cd $OLDPWD
+    shred /tmp/pwstorage_db.xml && rm /tmp/pwstorage_db.xml
 }
 
 function psyc-mod() {
 
     gpg --yes -o /tmp/pwstorage_db.xml -d $PSYCPATH/pwstorage_db.xml.gpg
-    nano /tmp/pwstorage_db.xml
+    $EDITOR /tmp/pwstorage_db.xml
     gpg --yes -o $PSYCPATH/pwstorage_db.xml.gpg -e -R $PSYCHOPATH /tmp/pwstorage_db.xml
-    shred -u /tmp/pwstorage_db.xml
+    shred /tmp/pwstorage_db.xml && rm /tmp/pwstorage_db.xml
 }
 
 alias psyc-see='gpg -d $PSYCPATH/pwstorage_db.xml.gpg | less '
-alias psyc-sync='cp -R $PSYCPATH/pwstorage_db.xml.gpg ~/Dropbox/Backup/pwstorage_db.xml.gpg'
+alias psyc-sync='cp -R $PSYCPATH/pwstorage_db.xml.gpg $MENTALBACKUP '
